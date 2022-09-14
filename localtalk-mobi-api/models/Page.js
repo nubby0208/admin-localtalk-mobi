5b83accb06d26fc395e969255bd7e5305d3e8a42
gruTonubby <109526476+gruTonubby@users.noreply.github.com>
2022-09-15 07:19:50 +0900

    localtalk-mobi-api seperated

--- localtalk-mobi-api/models/Page.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const PageSchema = new mongoose.Schema({
-  title: String,
-  slug: String,
-  content: String
-});
-
-module.exports = mongoose.model('Page', PageSchema);
